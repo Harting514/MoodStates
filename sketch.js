ed612d9c2c58c4118c0edde455b32b4ee848c074
Harting514 <69884552+Harting514@users.noreply.github.com>
2021-02-17 14:42:34 -0800

    finished Splash+Instructions Screen for Mood Drawings

--- sketch.js
+++ sketch.js
@@ -1,38 +1,43 @@
 /***********************************************************************************
-	SimpleStateMachine 
-	by Yik
+  SimpleStateMachine
+  by Yik
+  Template:
 
-	Template:
+  (1) Add your own PNG files in the assets folder. Make sure they match the names ***exactly*** of the existing PNGs.
+  (2) Add custom drawing code to drawSplash(), drawOne(), drawTwo(), drawThree(), drawFour(), drawFive()
+  (3) You can add your own interfaces - keys, mouse events, etc in the Interfaces section
 
-	(1) Add your own PNG files in the assets folder. Make sure they match the names ***exactly*** of the existing PNGs.
-	(2) Add custom drawing code to drawOne(), drawTwo(), drawThree(), drawFour(), drawFive()
-	(3) You can add your own interfaces - keys, mouse events, etc in the Interfaces section
-
-	Also start your localhost before running this, otherwise no PNGs will display
-
-------------------------------------------------------------------------------------
-	The way it works — you don't need to know this for the template use
-	* array of images gets loaded at startup
-	* drawFunction is a VARIABLE that points to a function varible name
-	* drawOne(), drawTwo(), etc. are set to be functions.
-	* the the keys 1-5 will change the drawFunction variable
+  Also start your localhost before running this, otherwise no PNGs will display
 
 ------------------------------------------------------------------------------------
-	Notes:
-	- a more advanced state machine with use array-indexing for each of
-		images the draw functions, but this is just for illustrative purposes
+  The way it works — you don't need to know this for the template use
+  * array of images gets loaded at startup
+  * drawFunction is a VARIABLE that points to a function varible name
+  * drawOne(), drawTwo(), etc. are set to be functions.
+  * the the keys 1-5 will change the drawFunction variable
+  * starts with drawSplash and waits for a mousePressed event
+  * adds a key, 's' to return to the splash screen
 
-	- even more advanced will be to put the draw functions into an array, would
-		be helpful for randomizing, go to the next function, etc
+------------------------------------------------------------------------------------
+  Notes:
+  - a more advanced state machine with use array-indexing variables for each of
+    images the draw functions, but this is just for illustrative purposes
 
-	- next step after that would be to put interfaces into an array that maps to
-		the functions
+  - even more advanced will be to put the draw functions into an array, would
+    be helpful for randomizing, go to the next function, etc
+
+  - next step after that would be to put interfaces into an array that maps to
+    the functions
 
 
 ***********************************************************************************/
 
-// Array of images
 var images = [];
+
+var strings = [];
+var midX;
+var startY;
+var lineHeight = 24;
 
 // variable that is a function 
 var drawFunction;
@@ -47,20 +52,24 @@ function preload() {
   images[2] = loadImage('assets/three.png');
   images[3] = loadImage('assets/four.png');
   images[4] = loadImage('assets/five.png');
-  images[5] = loadImage('assets/splash.png');
+  images[5] = loadImage('assets/Splash.png');
 }
 
 // Center drawing, drawFunction will be one for default
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  // Center our drawing objects
+  midX = width/2;
+  startY = 60;
+  // Center our drawing object
   imageMode(CENTER);
   textAlign(CENTER);
   textSize(24);
 
+
   // set to one for startup
   drawFunction = drawSplash;
+
 }
 
 // Very simple, sets the background color and calls your state machine function
@@ -78,6 +87,7 @@ drawOne = function() {
    image(images[0],width/2, height/2);
 
    fill(0,0,0);
+   text("So Mad!!!", width/2, height - 5* gTextOffset);
    text("State One", width/2, height - gTextOffset);
 }
 
@@ -86,6 +96,7 @@ drawTwo = function() {
    image(images[1],width/2, height/2);
 
    fill(240,120,0);
+   text("What is going on???", width/2, height - 5* gTextOffset);
    text("State Two", width/2, height - gTextOffset);
 }
 
@@ -94,6 +105,7 @@ drawThree = function() {
    image(images[2],width/2, height/2);
 
    fill(40,230,120);
+   text("ummmm...I don't know what to say", width/2, height - 5* gTextOffset);
    text("State Three", width/2, height - gTextOffset);
 }
 
@@ -102,6 +114,7 @@ drawFour = function() {
    image(images[3],width/2, height/2);
 
    fill(255,255,178);
+   text("Happy day, Happy life", width/2, height - 5* gTextOffset);
    text("State Four", width/2, height - gTextOffset);
 }
 
@@ -110,14 +123,40 @@ drawFive = function() {
    image(images[4],width/2, height/2);
 
    fill(230,50,50);
+   text("yowl~~~~~~, yowl~~~~~~", width/2, height - 5* gTextOffset);
    text("State Five", width/2, height - gTextOffset);
 }
 
 //-- drawSplash() will draw the image at index 4 from the array
 drawSplash = function() {
-   image(images[5],width/2, height/2);
+  image(images[5],width/2, height/2);
+  text("Click to continue", width/2, height - gTextOffset);
 }
 
+
+drawInstruction = function() {
+  loadArray();
+  image(images[0],width *1 /6, height/2,width/7,width/7);
+  image(images[1],width *2 /6, height/2,width/7,width/7);
+  image(images[2],width *3 /6, height/2,width/7,width/7);
+  image(images[3],width *4 /6, height/2,width/7,width/7);
+  image(images[4],width *5 /6, height/2,width/7,width/7);
+
+}
+
+
+
+function loadArray() {
+  strings[0] = "press number 1-5 to see  mood";
+  strings[1] = "";
+  strings[2] = "press 's' back to the Splash page";
+  strings[3] = "";
+  strings[4] = "press 'i' back to Instruction";
+  fill(255);
+  for( let i = 0 ; i < strings.length; i++ ) {
+    text( strings[i], midX, startY + (i * lineHeight) )
+  }
+}
 
 //========= TEMPLATE: add or change interface functions, as you like =========
 
@@ -127,30 +166,34 @@ function keyTyped() {
     return;
   }
 
-  if( key === '1' ) {
-    drawFunction = drawOne;
-  }
-  else if( key === '2' ) {
-    drawFunction = drawTwo;
-  }
-  else if( key === '3' ) {
-    drawFunction = drawThree;
-  }
-  else if( key === '4' ) {
-    drawFunction = drawFour;
-  }
-  else if( key === '5' ) {
-    drawFunction = drawFive;
-  }
-
-  else if( key === 's' ) {
-    drawFunction = drawSplash;
-  }
+    if( key === '1' ) {
+      drawFunction = drawOne;
+    }
+    else if( key === '2' ) {
+      drawFunction = drawTwo;
+    }
+    else if( key === '3' ) {
+      drawFunction = drawThree;
+    }
+    else if( key === '4' ) {
+      drawFunction = drawFour;
+    }
+    else if( key === '5' ) {
+      drawFunction = drawFive;
+    }
+    else if( key === 's' ) {
+      drawFunction = drawSplash;
+    }
+    else if( key === 'i' ) {
+      drawFunction = drawInstruction;
+    }
+  
 }
 
 function mousePressed() {
   // only change state if we are in splash screen
   if( drawFunction === drawSplash ) {
-    drawFunction = drawOne;
+    drawFunction = drawInstruction;
+
   }
 }
